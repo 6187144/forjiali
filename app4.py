5441f236f474cf5db8be0e717f5230549324374c
6187144 <6187144@cegep-heritage.qc.ca>
2022-03-21 13:48:33 -0400

    hopefully last version 1.1

--- app4.py
+++ app4.py
@@ -30,6 +30,19 @@ def getPage(pageKey):
         page["content"].append(result[4])
     return page
 
+def getImages():
+    productList=[]
+    cur = db.getConnection()
+    query = cur.execute("SELECT `file`,`description`,`price` FROM `gallery`")
+    results = cur.fetchall()
+    db.closeConn(cur)
+    for result in results:
+        # page["file"].append(result[0])
+        # page["description"].append(result[1])
+        # page["price"].append(result[2])
+        productList.append(result)
+    return productList
+
 @view_config(
     route_name='home',
     renderer="templates/home.jinja2"
@@ -60,7 +73,78 @@ def b1(request):
     return{"items":items,"pageTitle":pageTitle, "content":content, "pageId":pageId, "menuTitle":menuTitle, pageName:"pageName"}
     # return{page}
     
+@view_config(
+    route_name='b2',
+    renderer="templates/home.jinja2"
+) 
+def b2(request):
+    items=getMenu()
+    page = getPage("b2")
+    menuTitle = page.get("menu_title")[0]
+    pageTitle = page.get("page_title")[0]
+    pageId = page.get("pageId")[0]
+    pageName = page.get("page_name")[0]
+    content = page.get("content")[0]
+    return{"items":items,"pageTitle":pageTitle, "content":content, "pageId":pageId, "menuTitle":menuTitle, pageName:"pageName"}
+    # return{page}
 
+@view_config(
+    route_name='b3',
+    renderer="templates/home.jinja2"
+) 
+def b3(request):
+    items=getMenu()
+    page = getPage("b3")
+    menuTitle = page.get("menu_title")[0]
+    pageTitle = page.get("page_title")[0]
+    pageId = page.get("pageId")[0]
+    pageName = page.get("page_name")[0]
+    content = page.get("content")[0]
+    return{"items":items,"pageTitle":pageTitle, "content":content, "pageId":pageId, "menuTitle":menuTitle, pageName:"pageName"}
+
+@view_config(
+    route_name='b4',
+    renderer="templates/home.jinja2"
+) 
+def b4(request):
+    items=getMenu()
+    page = getPage("b4")
+    menuTitle = page.get("menu_title")[0]
+    pageTitle = page.get("page_title")[0]
+    pageId = page.get("pageId")[0]
+    pageName = page.get("page_name")[0]
+    content = page.get("content")[0]
+    return{"items":items,"pageTitle":pageTitle, "content":content, "pageId":pageId, "menuTitle":menuTitle, pageName:"pageName"}
+
+@view_config(
+    route_name='b5',
+    renderer="templates/home.jinja2"
+) 
+def b5(request): 
+    items=getMenu()
+    page = getPage("b5")
+    menuTitle = page.get("menu_title")[0]
+    pageTitle = page.get("page_title")[0]
+    pageId = page.get("pageId")[0]
+    pageName = page.get("page_name")[0]
+    content = page.get("content")[0]
+    return{"items":items,"pageTitle":pageTitle, "content":content, "pageId":pageId, "menuTitle":menuTitle, pageName:"pageName"}
+
+@view_config(
+    route_name='products',
+    renderer="templates/products.jinja2"
+) 
+def products(request):
+    items=getMenu()
+    products=getImages()
+    # page = getImages("products")
+    # file = page.get("file")[0]
+    # description = page.get("description")[0]
+    # price = page.get("price")[0]
+    # return{"items":items,"file":file, "description":description, "price":price}
+    # return{page}
+    return{"items":items,"products":products}
+ 
 if __name__ == '__main__':
     
     with Configurator() as config:
@@ -70,9 +154,16 @@ if __name__ == '__main__':
             path='static')
         config.add_static_view(name='img',
             path='img')
+        config.add_static_view(name='gal',
+            path='img/gallery')
 
         config.add_route('home', '/home')
         config.add_route('b1', '/b1')
+        config.add_route('b2', '/b2')
+        config.add_route('b3', '/b3')
+        config.add_route('b4', '/b4')
+        config.add_route('b5', '/b5')
+        config.add_route('products', '/products')
         # config.add_route('destination', '/Destination')
 
         config.scan()
